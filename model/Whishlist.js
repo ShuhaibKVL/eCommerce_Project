5ed3423d8323c764f1da81e029e88779cae3f881
ShuhaibKVL <139763626+ShuhaibKVL@users.noreply.github.com>
2024-03-02 11:03:09 +0530

    saved data to new branch

--- model/Whishlist.js
+++ model/Whishlist.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose')
 const { stringify } = require('uuid')
+const Product = require('../model/Product')
 
 const whishList = mongoose.Schema({
     UserId : {
         type:String,
         required:true
     },
-    ProductId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref : 'Product',
-        required:true
-    }
+    Product:[{
+        ProductId:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref : 'Product',
+            required:true
+        }
+    }]
 })
 
 module.exports = mongoose.model('whishList',whishList)
